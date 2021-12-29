1b881252e701ffdbb33cdee90c0654759ffaf830
Masterquiz <zribi.mehdi@gbgrassi.it>
2021-12-29 20:49:46 +0100

    contact page style & minor changes

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,18 +1,14 @@
-(function waitUntilLoad() {
-  if (document.querySelector(".ham")) {
-    const hamburger = document.querySelector(".ham");
-    const navsub = document.querySelector(".menu");
+function toggleMenu() {
+  const hamburger = document.querySelector(".ham");
+  const navsub = document.querySelector(".menu");
 
-    hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle("change");
-      navsub.classList.toggle("menu-change");
+  hamburger.classList.toggle("change");
+  navsub.classList.toggle("menu-change");
 
-      if (navsub.classList.contains("menu-change")) {
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  if (navsub.classList.contains("menu-change")) {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
-        window.onscroll = () => window.scrollTo(scrollLeft, scrollTop);
-      } else window.onscroll = {};
-    });
-  } else setTimeout(waitUntilLoad, 30);
-})();
+    window.onscroll = () => window.scrollTo(scrollLeft, scrollTop);
+  } else window.onscroll = {};
+};
